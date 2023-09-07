758133ba925f9f02b2fd1e16546cd0d446ddcd05
romanlavrinjuk <romanrostislavovich@gmail.com>
2023-09-07 18:55:43 +0200

    subject

--- todo-app-main/src/app/app.component.ts
+++ todo-app-main/src/app/app.component.ts
@@ -1,17 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { environment } from 'src/environments/environments';
 import { TaskService } from './services/task/task.service';
 import {FormArray, FormBuilder, FormControl, FormGroup, Validator, Validators} from "@angular/forms";
 import {AppValidators} from "./services/app-validators";
-import {takeUntil} from "rxjs";
+import {from, of, Subject, takeUntil} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = environment.title;
 
-  constructor(private taskService: TaskService) {}
+  private subject = new Subject();
+
+  constructor(private taskService: TaskService) {
+    this.subject.next('hello world');
+  }
+
+  ngOnInit() {
+    of('foo', 'baar').subscribe((v) => {
+      console.log(v);
+    })
+    from([1, 2]).subscribe((v) => {
+      console.log(v);
+    });
+    this.subject.subscribe((v) => {
+      console.log('sub1 ', v)
+    })
+    this.subject.subscribe((v) => {
+      console.log('sub2 ', v)
+    })
+  }
+
+  changeSubject() {
+    this.subject.next('RxJS')
+  }
+
 }
